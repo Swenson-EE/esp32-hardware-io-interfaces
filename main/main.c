aa0e285ac5690435efba8b1461f4fbaf69b7f4b2
swentyl <tylerjswenson262@gmail.com>
2025-05-15 08:54:31 -0500

    Information about project.

--- main/main.c
+++ main/main.c
@@ -1,4 +1,10 @@
-
+/*
+The goal of this project is to help me dive into different hardware io interfaces such as 
+USB 3.0/USB C
+Thunderbolt
+SATA
+PCIe
+*/
 
 #include <stdio.h>
 #include <inttypes.h>
